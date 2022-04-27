1cefbdf6b6c335ff21e242e59db4e68d53dda897
Niklas Rousset <niklas.rousset@gmail.com>
2022-04-28 01:11:09 +0200

    updated versions and test

--- frontend-public/src/App.test.js
+++ frontend-public/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
 import App from './App';
 
-/*test('renders learn react link', () => {
+test('paypal check', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/Preis/i);
   expect(linkElement).toBeInTheDocument();
-});*/
+});
