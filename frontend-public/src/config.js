abe6b1034b17d4f00c050197f139abebc3a76d4a
Niklas Rousset <niklas.rousset@gmail.com>
2022-07-19 18:30:00 +0200

    Moved api endpoint to config.js file, updated gitignore

--- frontend-public/src/config.js
+++ frontend-public/src/config.js
@@ -1,1 +1,2 @@
 export const PAYPAL_CLIENT_ID = "AWM3mAoQzWAPdQvnIXsNcLFfdRoGII8RZlP1N_ypQQYDLS3gOa-pVVVPNDDSHRiFRXVofub7w0a3fhCr";
+export const API_ENDPOINT = "https://tennisbooking.by-rousset.de/api.php/";
